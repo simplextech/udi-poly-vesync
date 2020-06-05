5af5348d39fe0b48df1454589fcd9d80800a9507
simplextech <jeubanks@simplextech.net>
2020-06-04 20:58:36 -0400

    1.0.7
    Adjustments for OpenADR /cmd/DON/<dim level>

--- nodes/vesync_bulb.py
+++ nodes/vesync_bulb.py
@@ -34,9 +34,19 @@ class VeSyncBulbNode(polyinterface.Node):
 
     def setOn(self, command):
         level = self.bulb.brightness
-        self.bulb.turn_on()
-        self.setDriver('GV1', 100, force=True)
-        self.setDriver('ST', level, force=True)
+        val = int(command['value'])
+        if val == 0:
+            self.bulb.turn_off()
+            self.setDriver('GV1', 0, force=True)
+            self.setDriver('ST', 0, force=True)
+        elif val > 0:
+            self.bulb.set_brightness(val)
+            self.setDriver('GV1', 100, force=True)
+            self.setDriver('ST', val, force=True)
+        else:
+            self.bulb.turn_on()
+            self.setDriver('GV1', 100, force=True)
+            self.setDriver('ST', level, force=True)
 
     def setOff(self, command):
         self.bulb.turn_off()
